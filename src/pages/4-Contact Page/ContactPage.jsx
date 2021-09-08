bfb11056b55b88b9b574ba9fe045d97727b23374
clecardona <clecardona@gmail.com>
2021-09-08 16:26:56 +0200

    contact page layout

--- src/pages/4-Contact Page/ContactPage.jsx
+++ src/pages/4-Contact Page/ContactPage.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import ContactDescriptionSection from "./ContactDescriptionSection";
+import FormContactSection from "./FormContactSection";
+import LocalisationSection from "./LocalisationSection";
 
 export default function ContactPage() {
-  return <main>contact</main>;
+  return (
+    <main className="page-contact">
+      <ContactDescriptionSection />
+      <FormContactSection />
+      <LocalisationSection />
+    </main>
+  );
 }
