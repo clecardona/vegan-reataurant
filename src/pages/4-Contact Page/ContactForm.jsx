60f07edefdf0e91c588df31ccb45e7fd4c6a97e5
clecardona <clecardona@gmail.com>
2021-09-10 14:14:17 +0200

    hiding api_key

--- src/pages/4-Contact Page/ContactForm.jsx
+++ src/pages/4-Contact Page/ContactForm.jsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import FormItem from "../../components/FormItem";
 
 export default function ContactForm() {
-  const moment = require("moment");
+  //const moment = require("moment");
   //const start = moment(Date.now()).format("YYYY-mm-DD");
 
   function doSomething(data) {
     //send data by mail to info@gmail.com
+    alert("well done");
   }
 
   //refactor inputs
@@ -41,20 +42,13 @@ export default function ContactForm() {
           label="pick a time"
           value={time}
         />
-        {/*  <label>
-          <p>Pick a date:</p>
-          <input
-            type="date"
-            placeholder="Pick a date"
-            min={start}
-            max="2021-12-31"
-          ></input>
-        </label> */}
-        {/* 
-        <label>
-          <p> At what time ? :</p>
-          <input type="time" value="13:30" placeholder="Pick an hour"></input>
-        </label> */}
+        <input
+          className="btn btn-main btn-send"
+          type="submit"
+          placeholder="Send"
+          /* disabled="false" */
+          value="Send"
+        />
       </form>
     </section>
   );
--- src/pages/4-Contact Page/Localisation components/Map.jsx
+++ src/pages/4-Contact Page/Localisation components/Map.jsx
@@ -4,10 +4,13 @@ import GoogleMapReact from "google-map-react";
 import MapMarker from "./MapMarker";
 
 export default function Map({ coordinates }) {
+  const ck = require("ckey");
+  const api_key = ck.REACT_APP_MAPS_API_KEY;
+
   return (
     <div className="map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyDM7qJpVBt4kzLZ5vlz5qrOEAl2M-CH3hU" }}
+        bootstrapURLKeys={{ key: api_key }}
         defaultCenter={coordinates}
         defaultZoom={15}
       >
