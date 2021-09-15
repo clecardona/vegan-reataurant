26ec30fcafbabdf5b12033ccac1f9aa03a5319db
clecardona <clecardona@gmail.com>
2021-09-15 09:13:33 +0200

    404 default case

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./styles/base.scss";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Page404 from "./components/404";
 import CategoryPage from "./components/Category/CategoryPage";
 import ContactPage from "./components/Contact/ContactPage";
 import HomePage from "./components/HomePage/HomePage";
@@ -9,32 +10,17 @@ import ProductPage from "./components/Product/ProductPage";
 import Header from "./components/shared/Header";
 import Footer from "./components/shared/Footer";
 
-/**
- * Im writting this here, because i cannot put comments inside a folder.
- * The pages/ folder is excelent.
- * I understand wanting to put 1-, 2-, 3-, etc to each page to view them in
- * their correct order.
- *
- * But please watch out for the empty spaces and upercases, this can lead to bugs
- * So do:
- * 1-home-page
- *
- * Instead of:
- * 1-Home page
- *
- * -1 because this may lead to bugs on older Operating Systems
- */
 export default function App() {
   return (
     <div className="App">
       <Router>
         <Header />
         <Switch>
-          {/* Why menu/category has an exact tag? I think is better to have a default case with an 404 error page if you dont find a match */}
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/menu/:category" component={CategoryPage} />
           <Route path="/menu/:category/:productID" component={ProductPage} />
+          <Route path="/menu/:category" component={CategoryPage} />
           <Route path="/contact" component={ContactPage} />
+          <Route path="/" component={Page404} />
         </Switch>
         <Footer />
       </Router>
