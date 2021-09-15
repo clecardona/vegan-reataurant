ed7ffba866fecff9a13c153503258e3b9a8168c9
clecardona <clecardona@gmail.com>
2021-09-15 10:38:54 +0200

    remove comments

--- src/components/shared/ButtonBack.jsx
+++ src/components/shared/ButtonBack.jsx
@@ -2,10 +2,6 @@ import React from "react";
 
 import arrowBack from "../../assets/icns/arrow-left-circle.svg";
 
-// I will not reduce points, but next time store the vector as a .svg image.
-// Anybody can open this component, mess with a single space between this long string and corrup the icon
-// I will reduce points next time -ok
-
 export default function ButtonBack({ label }) {
   return (
     <>
